373bfb41dcde4136c96a13ae8ecaf4c7e9d29434
Antonio Martín Ruiz <amr.antonio42@gmail.com>
2020-06-01 19:11:42 +0200

    Separación X e y

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import numpy as np
-import sklearn as skl
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+from sklearn import preprocessing
 
 np.random.seed(0)
 
 ############ Lectura de los datos
+
 description = "data/adult.names"
 attr = []
 with open(description, "r") as f:
@@ -13,22 +15,44 @@ with open(description, "r") as f:
             line = line.split()
             attr.append(line[1])
 
-df = pd.read_csv(
+df_tra = pd.read_csv(
     "data/adult.data",
     names=attr,
 )
 
+df_tes = pd.read_csv(
+    "data/adult.test",
+    names = attr        
+)
+
 
 print('Value : Number of diferent values : Number of missing values')
-for x in df.keys():
-    print(x, ':', len(set(df[x])), ':', len(df[df[x] == '?']))
+for x in df_tra.keys():
+    print(x, ':', len(set(df_tra[x])), ':', len(df_tra[df_tra[x] == '?']))
 
 
 
-print(df.shape)
-df = df.replace('?', np.nan) # Reemplazamos los valores ? por valores perdidos
+print(df_tra.shape)
+df_tra = df_tra.replace('?', np.nan) # Reemplazamos los valores ? por valores perdidos
+df_tes = df_tes.replace('?', np.nan)
 
-elem, cols = df.shape
-df_mode=df.mode()
-for x in df.columns.values:
-    df[x]=df[x].fillna(value=df_mode[x].iloc[0])
+elem, cols = df_tra.shape
+df_mode=df_tra.mode()
+for x in df_tra.columns.values:
+    df_tra[x]=df_tra[x].fillna(value=df_mode[x].iloc[0])
+    
+df_mode=df_tes.mode()
+for x in df_tes.columns.values:
+    df_tes[x]=df_tes[x].fillna(value=df_mode[x].iloc[0])
+
+# Separación de X e Y
+    
+Y_tra = df_tra.pop("Class")
+X_tra = df_tra
+Y_tes = df_tes.pop("Class")
+X_tes = df_tes
+
+# Random forest
+
+#clf = RandomForestClassifier()
+#clf.fit(X_tra, Y_tra)
