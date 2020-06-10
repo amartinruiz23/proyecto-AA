f9b24695b976b60b99dc3f65bd9500a9386281b5
Antonio Martín Ruiz <amr.antonio42@gmail.com>
2020-06-10 20:38:23 +0200

    Función para sustituir valores perdidos categoricos

--- src/main.py
+++ src/main.py
@@ -4,10 +4,61 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import cross_validate
+from collections import Counter
 
 np.random.seed(0)
 
-############ Lectura de los datos
+# --- Funciones auxiliares --- 
+
+def replace_lost_categorical_values(df):
+    
+    # Obtenemos las columnas con variables categóricas
+    cols = df.columns
+    num_cols = df._get_numeric_data().columns
+    ind = list(set(cols) - set(num_cols))
+    
+    # Iteramos sobre estas columnas para calcular su distribución de probabilidad
+    # y asignar un valor a los valores perdidos.
+    
+    for i in ind:
+        count = Counter(df[i]) # Cuenta las apariciones de cada clase
+        acumulated = {} # Diccionario en el que almacenaremos los valores acumulados
+        total = 0
+        for x in count: # Rellenamos el diccionario con los valores acumulados
+            if x != '?':
+                total += count[x]
+                acumulated[x] = total
+        acumulated = {k : v / total for k, v in acumulated.items()} # Normalizamos
+        
+        # Asignamos ahora un valor a los valores perdidos
+        for j in range(len(df[i])):
+           if df[i][j] == '?':
+               prob = np.random.uniform()
+               less = {}
+               for y in acumulated:
+                   if prob <= acumulated[y]:
+                       less[y] = acumulated[y]
+               df[i][j] = min(less, key=less.get)
+
+    return df        
+
+def class_division(filename, attr):
+    df = pd.read_csv(
+        filename,
+    names = attr
+    )
+    df_mode=df.mode()
+    for x in df.columns.values:
+        df[x]=df[x].fillna(value=df_mode[x].iloc[0])
+    
+    df = replace_lost_categorical_values(df)
+    df = df.replace('?', np.nan)
+    y = df.pop("Class")
+    #df = pd.get_dummies(df)
+    return df, y
+
+
+# --- Lectura de los datos ---
 
 description = "data/adult.names"
 attr = []
@@ -17,25 +68,14 @@ with open(description, "r") as f:
             line = line.split()
             attr.append(line[1])
 
-def class_division(filename, attr):
-    df = pd.read_csv(
-        filename,
-    names = attr        
-    )
-    df = df.replace('?', np.nan)
-    df_mode=df.mode()
-    for x in df.columns.values:
-        df[x]=df[x].fillna(value=df_mode[x].iloc[0])
-
-    y = df.pop("Class")
-    df = pd.get_dummies(df)
-    return df, y
-
 X, y = class_division("data/adult.data", attr)
 
+    
+#print(X)
 
-print(X)
+# --- Random forest ---
 
+"""
 clf = RandomForestClassifier()
 # clf.fit(X, y)
 # pred_tra = clf.predict(X)
@@ -43,3 +83,4 @@ clf = RandomForestClassifier()
 #print("E_tra: ", clf.score(X, y))
 cv_results = cross_validate(clf, X, y, cv=5,)
 print("E_cv: ", sum(cv_results['test_score'])/len(cv_results['test_score']) )
+"""
