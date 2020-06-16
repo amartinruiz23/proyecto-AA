514d516764e575fd93734b1212e48c0118ac83cc
pedrobn23 <pedrobonillanadal@hotmail.com>
2020-06-16 17:58:40 +0200

    solved

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ tam = info_size(X, 'Tamaño de los datos después de encode:')
 
 preprocesado = pl.make_pipeline(fs.VarianceThreshold(threshold=0.01),
                                 sklpre.StandardScaler(),
-                                skld.PCA(n_components=tam - 1))
+                                skld.PCA(tol=0.01))
 
 preprocesado.fit(X)
 X = preprocesado.transform(X)
